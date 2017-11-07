a03edd548edd362e8f700ce296057cc43cdb01b8
myk <maiklasm@gmail.com>
2017-11-07 19:28:39 +0100

    Remove proxy

--- src/features/chuckFacts/searchFactsEpic.js
+++ src/features/chuckFacts/searchFactsEpic.js
@@ -4,13 +4,15 @@ import { searchFactsSuccess, searchFactsFailure } from './actions';
 import { CHUCK_FACTS } from './actionTypes';
 import { ajax } from 'rxjs/observable/dom/ajax';
 
+const host = 'https://api.chucknorris.io';
+
 const searchFactsEpic = action$ => {
   return action$.ofType(CHUCK_FACTS.SEARCH_FACTS).switchMap(({ query }) => {
     if (!query.trim()) {
       return Observable.of(searchFactsSuccess([]));
     }
 
-    return ajax({ url: `/jokes/search?query=${query}`, crossDomain: true })
+    return ajax({ url: `${host}/jokes/search?query=${query}`, crossDomain: true })
       .map(payload => searchFactsSuccess(payload.response))
       .startWith({ type: CHUCK_FACTS.SEARCH_FACTS_REQUEST })
       .catch(error => Observable.of(searchFactsFailure(error)));
